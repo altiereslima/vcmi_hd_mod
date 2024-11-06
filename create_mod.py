7f181eddfa37b29d146663babc2b3ce2ddb929ce
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2024-11-06 23:27:08 +0100

    handle sprites

--- create_mod.py
+++ create_mod.py
@@ -23,9 +23,11 @@
 # SOFTWARE.
 
 import os
+import io
 import json
 import zipfile
 import pandas as pd
+from PIL import Image
 
 def create_mod(in_folder, out_folder):
     out_folder = os.path.join(out_folder, "hd_version")
@@ -60,10 +62,33 @@ def create_mod(in_folder, out_folder):
             with zipfile.ZipFile(os.path.join(destination, "content.zip"), mode="a") as archive:
                 path = os.path.join(in_folder, name, lang if "loc" in name else "")
                 for folder in os.listdir(path):
-                    for file in os.listdir(os.path.join(path, folder)):
-                        archive.writestr("sprites/" + folder + "$" + scale + "/" + file, open(os.path.join(path, folder, file), "rb").read())
-                    archive.writestr("sprites/" + folder + "$" + scale + ".json", create_animation_config(folder + "$" + scale, os.listdir(os.path.join(path, folder)), df))
+                    handle_sprites(archive, path, folder, scale, df)
 
+def handle_sprites(archive, path, folder, scale, df):
+    data = {x:open(os.path.join(path, folder, x), "rb").read() for x in os.listdir(os.path.join(path, folder))}
+
+    # water + rivers special handling - paletteAnimation
+    if folder.upper() in ["WATRTL", "LAVATL"] + ["CLRRVR", "MUDRVR", "LAVRVR"]:
+        data = {str.replace(x, "_0", ""):y for x, y in data.items() if str.endswith(x, "_0.png")}
+
+    ## resize def - TODO: for town structures (offsets needed)
+    #max_size_x = 0
+    #max_size_y = 0
+    #for name, item in data.items():
+    #    img = Image.open(io.BytesIO(item))
+    #    max_size_x = max(max_size_x, img.width)
+    #    max_size_y = max(max_size_y, img.height)
+    #for item in data.keys():
+    #    img = Image.open(io.BytesIO(data[item]))
+    #    tmpimg = Image.new(img.mode, (max_size_x, max_size_y), (255, 255, 255, 0))
+    #    tmpimg.paste(img, (0, 0))
+    #    img_byte_arr = io.BytesIO()
+    #    tmpimg.save(img_byte_arr, format='PNG')
+    #    data[item] = img_byte_arr.getvalue()
+
+    for file, content in data.items():
+        archive.writestr("sprites/" + folder + "$" + scale + "/" + file, content)
+    archive.writestr("sprites/" + folder + "$" + scale + ".json", create_animation_config(folder + "$" + scale, data.keys(), df))
 
 def create_mod_config():
     conf = {
